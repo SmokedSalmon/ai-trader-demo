4c7447fa203787676f908594d1c0ca1242cece93
SmokedSalmon <jerrysu.jie@gmail.com>
2026-01-04 13:49:20 +0800

    disable build type-check

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  // Disable type-check here as it is just a demo project
+  // enable build time type-check for serious projects.
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
